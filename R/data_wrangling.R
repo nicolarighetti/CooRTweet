2cef8292e838546eb62fe682d07a2c262d50a15b
Nicola Righetti <nicolarighetti79@gmail.com>
2022-09-11 15:08:40 +0200

    edit error message get_clsb get_cotweet

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -77,7 +77,7 @@ data_wrangling <- function(tweets,
       dplyr::filter(is.na(type))
 
     if (nrow(dset_rt) == 0) {
-      message("\n### No network detected ####\n")
+      message("\n### No original tweets in the dataset ####\n")
       opt <- options(show.error.messages = FALSE)
       on.exit(options(opt))
       stop()
@@ -121,7 +121,7 @@ data_wrangling <- function(tweets,
       dplyr::filter(is.na(type))
 
       if (nrow(dset_rt) == 0) {
-        message("\n### No network detected ####\n")
+        message("\n### No original tweets in the dataset ####\n")
         opt <- options(show.error.messages = FALSE)
         on.exit(options(opt))
         stop()
