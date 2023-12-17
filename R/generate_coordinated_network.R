3d44bd91d9bde69a9ae8ba0a2ae6c95bb13d66d7
Nicola Righetti <nicolarighetti79@gmail.com>
2023-12-17 10:52:20 +0100

    edge_simmetry_score
    The graph now incorporates the contribution of each node within the pair to the pair's edge weight, specifically, the number of shared content_id that contribute to the edge weight. Additionally, an edge_symmetry_score is included, which equals 1 in cases of equal contributions from both users and approaches 0 as the contributions become increasingly unequal. This score, along with the value of contributions, can be utilized for further filtering or examining cases where the score is particularly low. Working with an undirected graph, it is plausible that the activity of highly active users disproportionately affects the weight of edges connecting them to less active users. For instance, if user A shares the same objects (object_id) 100 times, and user B shares the same object only once, but within a time frame that matches the time_window defined in the parameter for all of user A's 100 shares, then the edge weight between A and B will be 100, although this weight is almost entirely influenced by the hyperactivity of user A. The edge_symmetry_score, along with the counts of shares by each user (n_content_id and n_content_id_y), allows for monitoring and controlling this phenomenon

--- R/generate_coordinated_network.R
+++ R/generate_coordinated_network.R
@@ -17,6 +17,20 @@
 #' high number of objects (for instance, more than 99% of user pairs in the network) in the same time
 #' window.
 #'
+#' The graph also incorporates the contribution of each node within the pair to the pair's edge weight,
+#' specifically, the number of shared `content_id` that contribute to the edge weight. Additionally, an
+#' `edge_symmetry_score` is included, which equals 1 in cases of equal contributions from both users and
+#' approaches 0 as the contributions become increasingly unequal. This score, along with the value of
+#' contributions, can be utilized for further filtering or examining cases where the score is particularly
+#' low. Working with an undirected graph, it is plausible that the activity of highly active users
+#' disproportionately affects the weight of edges connecting them to less active users. For instance,
+#' if user A shares the same objects (`object_id`) 100 times, and user B shares the same object only once,
+#' but within a time frame that matches the `time_window` defined in the parameter for all of user A's
+#' 100 shares, then the edge weight between A and B will be 100, although this weight is almost entirely
+#' influenced by the hyperactivity of user A. The `edge_symmetry_score`, along with the counts of shares
+#' by each user `user_id` and `user_id_y` (`n_content_id` and `n_content_id_y`), allows for monitoring
+#' and controlling this phenomenon.
+#'
 #' @param x a data.table (result from `detect_coordinated_groups`) with the Columns: `object_id`, `id_user`, `id_user_y`, `content_id`, `content_id_y`, `timedelta`
 #' @param fast_net If the data.table x has been updated with the restrict_time_window function and this parameter is set to TRUE, two columns weight_full and weight_fast are created, the first containing the edge weights of the full graph, the second those of the subgraph that includes the shares made in the narrower time window.
 #' @param edge_weight The edges with weight that exceeds a threshold are marked with 0 (not exceeding) or 1 (exceeding).
@@ -33,7 +47,8 @@
 #' time window established with the restrict_time_window function (fast subgraph).
 #' The default value is 0, meaning that no subgraph is created.
 #'
-#' @return A weighted, undirected network (igraph object) where the vertices (nodes) are users and edges (links) are the membership in coordinated groups (`object_id`)
+#' @return A weighted, undirected network (igraph object) where the vertices (nodes) are users and
+#' edges (links) are the membership in coordinated groups (`object_id`).
 #'
 #' @references
 #' Giglietto, F., Righetti, N., Rossi, L., & Marino, G. (2020). It takes a village to manipulate the media: coordinated link sharing behavior during 2018 and 2019 Italian elections. *Information, Communication & Society*, 23(6), 867-891.
@@ -68,10 +83,29 @@ generate_coordinated_network <- function(x, fast_net = FALSE, edge_weight = 0.5,
     # standardize the order of the vertices
     x[, `:=`(id_user = pmin(id_user, id_user_y), id_user_y = pmax(id_user, id_user_y))]
 
-    # Aggregate edges and compute weight
-    x_aggregated <- x[, .(weight = .N,
-                          avg_time_delta = mean(time_delta)),
-                      by = .(id_user, id_user_y)]
+    # Aggregate edges and compute weight and edge_symmetry_score
+    x_aggregated <- x[, .(
+        # weight: Counts the number of connections (edges) between each pair of users
+        weight = .N,
+
+        # avg_time_delta: Calculates the average time difference across all connections for each user pair
+        avg_time_delta = mean(time_delta),
+
+        # n_content_id: Counts the number of unique content_ids for each user pair
+        n_content_id = uniqueN(content_id),
+
+        # n_content_id_y: Counts the number of unique content_id_ys for each user pair
+        n_content_id_y = uniqueN(content_id_y),
+
+        # edge_symmetry_score: Computes a score representing the symmetry of content sharing between users.
+        # This score is 1 when the sharing is perfectly symmetrical (equal contributions from both users),
+        # and approaches 0 as the contribution becomes more unequal.
+                          edge_symmetry_score = {
+                              n_cid = uniqueN(content_id)
+                              n_cidy = uniqueN(content_id_y)
+                              min(n_cid, n_cidy) / max(n_cid, n_cidy)
+                          }),
+                      by = .(id_user, id_user_y)] # Grouping by pairs of users for the aggregation
 
     # Create the graph with edge weights
     coord_graph <- graph_from_data_frame(x_aggregated, directed = FALSE)
