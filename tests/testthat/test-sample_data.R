c548e2faccd9732814fe5486086ab53116e9aba7
Nicola Righetti <nicolarighetti79@gmail.com>
2023-12-21 15:17:37 +0100

    edit
    edit (the test is now OK)

--- tests/testthat/test-sample_data.R
+++ tests/testthat/test-sample_data.R
@@ -14,13 +14,14 @@ testthat::test_that("Sample dataset is consistent", {
 testthat::test_that("Self-coordinated posts are filtered out", {
     result <- detect_groups(russian_coord_tweets,
         min_participation = 5,
-        time_window = 10
+        time_window = 10,
+        remove_loops = TRUE
     )
 
     coord_graph <- generate_coordinated_network(result)
 
     # Generate summary of user statistics
-    summary_users <- user_stats(result, weight_threshold = "none")
+    summary_users <- user_stats(coord_graph, weight_threshold = "none")
     user_self_coord <- summary_users[account_id == "9fa51ef17278c01d13d313741eddfc0b"]
     testthat::expect_equal(nrow(user_self_coord), 0)
 })
@@ -28,13 +29,14 @@ testthat::test_that("Self-coordinated posts are filtered out", {
 testthat::test_that("Self-coordinated posts are not filtered out", {
     result <- detect_groups(russian_coord_tweets,
                             min_participation = 5,
-                            time_window = 10
+                            time_window = 10,
+                            remove_loops = FALSE
     )
 
     coord_graph <- generate_coordinated_network(result)
 
     # Generate summary of user statistics
-    summary_users <- user_stats(result, weight_threshold = "none")
+    summary_users <- user_stats(coord_graph, weight_threshold = "none")
     user_self_coord <- summary_users[account_id == "9fa51ef17278c01d13d313741eddfc0b"]
     testthat::expect_gt(nrow(user_self_coord), 0)
 })
