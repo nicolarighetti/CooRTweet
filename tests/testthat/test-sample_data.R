2e9fa9cd3d1e9cb8dd562bd716d6857683ec5465
Nicola Righetti <nicolarighetti79@gmail.com>
2024-01-13 13:04:28 +0100

    edit
    tests have been edited to match the latest updates

--- tests/testthat/test-sample_data.R
+++ tests/testthat/test-sample_data.R
@@ -21,7 +21,7 @@ testthat::test_that("Self-coordinated posts are filtered out", {
     coord_graph <- generate_coordinated_network(result)
 
     # Generate summary of account statistics
-    summary_accounts <- account_stats(coord_graph, weight_threshold = "none")
+    summary_accounts <- account_stats(coord_graph, result, weight_threshold = "none")
     account_self_coord <- summary_accounts[account_id == "9fa51ef17278c01d13d313741eddfc0b"]
     testthat::expect_equal(nrow(account_self_coord), 0)
 })
@@ -36,7 +36,7 @@ testthat::test_that("Self-coordinated posts are not filtered out", {
     coord_graph <- generate_coordinated_network(result)
 
     # Generate summary of account statistics
-    summary_accounts <- account_stats(coord_graph, weight_threshold = "none")
+    summary_accounts <- account_stats(coord_graph, result, weight_threshold = "none")
     account_self_coord <- summary_accounts[account_id == "9fa51ef17278c01d13d313741eddfc0b"]
     testthat::expect_gt(nrow(account_self_coord), 0)
 })
